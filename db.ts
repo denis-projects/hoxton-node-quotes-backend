b7c8d93e00ce0ec2c0f6fd16d9b8bbb1119be625
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-10 21:23:58 +0100

    adding a database

---
+++ db.ts
@@ -0,0 +1,41 @@
+type Quotes = {
+    id: number
+    philosopher: string
+    philosopy: string
+}
+
+export const quotes: Quotes[] = [
+    {
+        id: 1,
+        philosopher: 'Nico',
+        philosopy: 'Learn all the modules.'
+    },
+    {
+        id: 2,
+        philosopher: 'Ed',
+        philosopy: 'Do not forget to push your work on time.'
+    },
+    {
+        id: 3,
+        philosopher: 'Denis',
+        philosopy: 'How to learn faster, without pratice too much :P.'
+    },
+    {
+        id: 4,
+        philosopher: 'React',
+        philosopy: 'If you learn me well, you will be a good developer.'
+    },
+    {
+        id: 5,
+        philosopher: 'JavaScript',
+        philosopy: ' If you think you are done with me, you are wrong. i will chase you even in your dreams.'
+    },
+    {
+        id: 6,
+        philosopher: 'TypeScript',
+        philosopy: 'If you did not give me the type i want, i will push the button that will destroy all code.'
+    }
+]
+
+
+
