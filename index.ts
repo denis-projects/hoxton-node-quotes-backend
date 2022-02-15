4f0903e686cba16182b26298eea1f66af183d117
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-15 08:45:54 +0100

    adding the features required fot backend

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import { quotes } from './db'
+import { Quotes } from './db'
 
 const app = express()
 
+app.use(cors())
+app.use(express.json());
+
+
 const PORT = 1234
 
-app.use(cors())
+// Get Homepage
 
 app.get('/', (req, res) => {
     res.send(`
@@ -19,9 +24,16 @@ app.get('/', (req, res) => {
    `)
 })
 
+
+
+// Get all quotes
+
 app.get('/quotes', (req, res) => {
     res.send(quotes)
 })
+
+
+// Get a random Quote
 
 app.get('/randomQuote', (req, res) => {
     const randomIndex = Math.floor(Math.random() * quotes.length)
@@ -29,6 +41,79 @@ app.get('/randomQuote', (req, res) => {
     res.send(quote)
 })
 
+
+
+// Get a quote by ID
+
+app.get('/quotes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const match = quotes.find((quote) => quote.id === id);
+    if (match) {
+        res.send(match);
+    } else {
+        res.status(404).send({ error: 'Quote not found.' });
+    }
+});
+
+
+
+// Add another philospy (POST)
+
+app.post('/quotes', (req, res) => {
+
+    const philosopy = req.body.philosopy;
+    const philosopher = req.body.philosopher;
+    const age = req.body.age;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const image = req.body.img;
+
+
+    const errors = [];
+
+
+    if (typeof philosopy !== 'string') {
+        errors.push('Check the typeof philosopy')
+    }
+    if (typeof philosopher !== 'string') {
+        errors.push('Check the typeof philosopher')
+    }
+    if (typeof firstName !== 'string') {
+        errors.push('Check the typeof firstName')
+    }
+    if (typeof lastName !== 'string') {
+        errors.push('Check the typeof lastName')
+    }
+
+    if (typeof image !== 'string') {
+        errors.push('Check the typeof image')
+    }
+
+
+
+    if (errors.length === 0) {
+
+        const newPhilosopy: Quotes = {
+            id: Math.random(),
+            philosopher: philosopher,
+            philosopy: philosopy,
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            image: image
+        };
+
+        quotes.push(newPhilosopy);
+        res.status(201).send(newPhilosopy);
+    } else {
+        res.status(400).send({ errors: errors });
+    }
+});
+
+
+
+
+// Listen the server 
 
 app.listen(PORT, () => {
     console.log(`Server is ready and running on: http://localhost:${PORT}/quotes`)
