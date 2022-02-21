f2c617f01440088bf8f60e0513ef93611becb723
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-21 09:20:13 +0100

    comit

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import Database from 'better-sqlite3'
 
 const app = express()
 
@@ -8,10 +9,158 @@ app.use(express.json());
 
 const PORT = 1234
 
-import quoteRouter from './resorces/quotes'
+// import quoteRouter from './resorces/quotes'
 
-app.use('/quotes', quoteRouter)
+// app.use('/quotes', quoteRouter)
 
+
+const db = new Database('./data.db', {
+    verbose: console.log
+})
+
+const getAllQuotes = db.prepare(`
+SELECT * FROM quotes;
+`)
+
+const getQuotesById = db.prepare(`
+SELECT * FROM quotes WHERE Id=? ;
+`)
+
+const createQuote = db.prepare(`
+INSERT INTO quotes (philosopy, philosopher, firstName, lastName, age, image ) VALUES (?, ?, ?, ?, ?. ? );
+`)
+
+
+const updateQuote = db.prepare(`
+UPDATE quotes SET philosopy=?, philosopher=?, firstName=?, lastName=?, age=?, image=?, WHERE id=?
+`)
+
+const deleteQuote = db.prepare(`
+DELETE FROM quotes WHERE id=?;
+`)
+
+// Get all quotes
+
+app.get('/quotes', (req, res) => {
+    const allQuotes = getAllQuotes.all()
+    res.send(allQuotes)
+})
+
+
+
+// Get a random Quote
+
+app.get('/randomQuote', (req, res) => {
+    const allQuotes = getAllQuotes.all()
+    const randomIndex = Math.floor(Math.random() * allQuotes.length)
+    res.send(allQuotes[randomIndex])
+})
+
+
+
+// Get a quote by ID
+
+app.get('/quotes/:id', (req, res) => {
+    const id = req.params.id;
+    const match = getQuotesById.get(id);
+
+    if (match) {
+        res.send(match);
+    } else {
+        res.status(404).send({ error: 'Quote not found.' });
+    }
+});
+
+
+
+// Add another philospy (POST)
+
+app.post('/quotes', (req, res) => {
+
+    const philosopy = req.body.philosopy;
+    const philosopher = req.body.philosopher;
+    const age = req.body.age;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const image = req.body.image;
+
+
+    const errors = [];
+
+    if (typeof philosopy !== 'string') {
+        errors.push('Check the typeof philosopy')
+    }
+    if (typeof philosopher !== 'string') {
+        errors.push('Check the typeof philosopher')
+    }
+    if (typeof firstName !== 'string') {
+        errors.push('Check the typeof firstName')
+    }
+    if (typeof lastName !== 'string') {
+        errors.push('Check the typeof lastName')
+    }
+    if (typeof image !== 'string') {
+        errors.push('Check the typeof image')
+    }
+    if (typeof age !== 'number') {
+        errors.push('Check the typeof image')
+    }
+
+
+
+    if (errors.length === 0) {
+        const create = createQuote.run(philosopher, philosopy, firstName, lastName, age, image)
+
+        res.status(201).send(create);
+    } else {
+        res.status(400).send({ errors: errors });
+    }
+});
+
+
+// Update the qutote
+
+app.patch('/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    // happy path: every key given exists and is the right type // ✅
+    // sad path: wrong keys or wrong types provided by user // ❌
+
+    const quotesToChange = quotes.find((quote) => quote.id === id);
+
+    if (quotesToChange) {
+
+        if (typeof req.body.philosopher === 'string') quotesToChange.philosopher = req.body.philosopher;
+        if (typeof req.body.philosopy === 'string') quotesToChange.philosopy = req.body.philosopy;
+        if (typeof req.body.image === 'string') quotesToChange.image = req.body.image;
+        if (typeof req.body.firstName === 'string') quotesToChange.firstName = req.body.firstName;
+        if (typeof req.body.lastName === 'string') quotesToChange.lastName = req.body.lastNames;
+        if (typeof req.body.age === 'number') quotesToChange.age = req.body.age;
+
+        res.send(quotesToChange);
+    } else {
+        res.status(404).send({ error: 'Philosopy not found.' });
+    }
+});
+
+
+// Delete a quote
+
+app.delete('/quotes/:id', (req, res) => {
+
+    // get id
+    const id = req.params.id;
+    const result = deleteQuote.run(id)
+
+
+
+    // delete if it exists
+    if (result.changes !== 0) {
+        res.send({ message: 'Quote deleted successfully.' });
+    } else {
+        res.status(404).send({ error: 'Quote not found.' });
+    }
+});
 
 // Listen the server 
 
---
+++ resorces/setup.ts
@@ -0,0 +1,111 @@
+import Database from 'better-sqlite3'
+// import { Router } from 'express'
+
+
+const db = new Database('./data.db', {
+    verbose: console.log
+})
+
+// const router = Router()
+
+type Quotes = {
+    id: number
+    philosopher: string
+    philosopy: string
+    age: number
+    firstName: string
+    lastName: string
+    image: string
+}
+
+let quotes: Quotes[] = [
+    {
+        id: 1,
+        philosopher: 'Nico',
+        philosopy: 'Learn all the modules.',
+        age: 34,
+        firstName: "Nicolas",
+        lastName: "Marcora",
+        image: "nicolas.jpg"
+    },
+    {
+        id: 2,
+        philosopher: 'Ed',
+        philosopy: 'Do not forget to push your work on time.',
+        age: 28,
+        firstName: "Edgar",
+        lastName: "Putans",
+        image: "ed.jpg"
+    },
+    {
+        id: 3,
+        philosopher: 'Denis',
+        philosopy: 'How to learn faster, without pratice too much :P.',
+        age: 28,
+        firstName: "Denis",
+        lastName: "Mimini",
+        image: "denis.jpg"
+    },
+    {
+        id: 4,
+        philosopher: 'React',
+        philosopy: 'If you learn me well, you will be a good developer.',
+        age: 11,
+        firstName: "React",
+        lastName: "Facebook",
+        image: "denis.jpg"
+    },
+    {
+        id: 5,
+        philosopher: 'JavaScript',
+        philosopy: ' If you think you are done with me, you are wrong. i will chase you even in your dreams.',
+        age: 27,
+        firstName: "JavaScript",
+        lastName: "Language",
+        image: "denis.jpg"
+    },
+    {
+        id: 6,
+        philosopher: 'TypeScript',
+        philosopy: 'If you did not give me the type i want, i will push the button that will destroy all code.',
+        age: 3,
+        firstName: "TypeScript",
+        lastName: "JavaScript",
+        image: "denis.jpg"
+    }
+]
+
+
+const dropTable = db.prepare('DROP TABLE IF EXIST quotes;');
+dropTable.run();
+
+const createQuotesTable = db.prepare(`
+CREATE TABLE IF NOT EXISTS quotes (
+    id INTEGER,
+    philosopy TEXT NOT NULL,
+    philosopher TEXT NOT NULL,
+    firstName TEXT NOT NULL,
+    lastName TEXT NOT NULL,
+    age INTEGER,
+    image TEXT NOT NULL,
+    PRIMARY KEY (id)
+    
+);
+`);
+
+createQuotesTable.run()
+
+
+const createQuote = db.prepare(`
+INSERT INTO quotes (philosopy, philosopher, firstName, lastName, age, image ) VALUES  (?, ?, ?, ?, ?, ?, ?);
+`);
+
+for (const quote of quotes) {
+    createQuote.run(quote.philosopy, quote.philosopher, quote.firstName, quote.lastName, quote.age, quote.image)
+}
+
+
+
+
+
+// export default router
